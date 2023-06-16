307a78c8457f81052edbffd82e1b848d183a3304
VinayKumar2004 <94241385+VinayKumar2004@users.noreply.github.com>
2023-06-16 13:13:33 +0530

    Create JSAssignment.js

---
+++ JSAssignment.js
@@ -0,0 +1,53 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+let nftCollection = []; 
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (name, eyeColor, shirtType, bling) {
+    const nft = {
+        name: name,
+        eyeColor: eyeColor,
+        shirtType: shirtType,
+        bling: bling
+      };
+    
+      nftCollection.push(nft);
+    
+}
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+    nftCollection.forEach((nft, index) => {
+        console.log(`NFT ${index + 1}:`);
+        console.log("Name: " + nft.name);
+        console.log("Eye Color: " + nft.eyeColor);
+        console.log("Shirt Type: " + nft.shirtType);
+        console.log("Bling: " + nft.bling);
+        console.log("---------------------------");
+      });
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+    return nftCollection.length;
+}
+
+// call your functions below this line
+mintNFT("NFT 1", "Blue", "T-shirt", "Gold Chain");
+mintNFT("NFT 2", "Green", "Hoodie", "Diamond Ring");
+mintNFT("NFT 3", "Red", "Tank Top", "Platinum Watch");
+
+listNFTs();
+
+console.log("Total NFTs minted: " + getTotalSupply());
