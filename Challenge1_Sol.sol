ef1aee68ae18d60cf81122e07492e7efab30a02d
VinayKumar2004 <94241385+VinayKumar2004@users.noreply.github.com>
2023-06-27 13:26:29 +0530

    Challenge1_Sol.sol

--- Challenge1_Sol.sol
+++ Challenge1_Sol.sol
@@ -1,1 +1,46 @@
+// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
+contract SimpleContract {
+    uint public myInteger;
+    string public myString;
+    bool public myBoolean;
+    address public myAddress;
+
+    function setInteger(uint newValue) public returns (uint) {
+        myInteger = newValue;
+        return myInteger;
+    }
+
+    function getInteger() public view returns (uint) {
+        return myInteger;
+    }
+
+    function setString(string memory newValue) public returns (string memory) {
+        myString = newValue;
+        return myString;
+    }
+
+    function getString() public view returns (string memory) {
+        return myString;
+    }
+
+    function setBoolean(bool newValue) public returns (bool) {
+        myBoolean = newValue;
+        return myBoolean;
+    }
+
+    function getBoolean() public view returns (bool) {
+        return myBoolean;
+    }
+
+    function setAddress(address newValue) public returns (address) {
+        myAddress = newValue;
+        return myAddress;
+    }
+
+    function getAddress() public view returns (address) {
+        return myAddress;
+    }
+}
